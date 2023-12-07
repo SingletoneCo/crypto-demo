22be833b9db534d45cfaedefac043c81778c5d63
aleksandra <singletone.co@gmail.com>
2023-12-07 19:28:04 +0100

    Updated paths mapping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SHELL } from '@crypto-demo/shell';
-import { CURRENCY } from '@crypto-demo/currency';
+import { SHELL } from '@shell';
+import { CURRENCY } from '@currency';
 
 
 @Component({
