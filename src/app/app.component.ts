35dcd80f88a1e71ef68f9f9624ee9f55743a7516
aleksandra <singletone.co@gmail.com>
2023-12-07 19:20:44 +0100

    Main architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SHELL } from '@crypto-demo/shell';
+import { CURRENCY } from '@crypto-demo/currency';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    SHELL,
+    CURRENCY
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/currency/index.ts
@@ -0,0 +1,10 @@
+import { CurrencyListComponent } from './currency-list/currency-list.component';
+import { CurrencyDetailsComponent } from './currency-details/currency-details.component';
+
+export { CurrencyListComponent } from './currency-list/currency-list.component';
+export { CurrencyDetailsComponent } from './currency-details/currency-details.component';
+
+export const CURRENCY = [
+  CurrencyListComponent,
+  CurrencyDetailsComponent
+];
